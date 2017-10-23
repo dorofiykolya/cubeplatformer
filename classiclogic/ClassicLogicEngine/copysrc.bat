1550accae9e6aec0bb3ba94679e599f4c80f413e
dorofii <dorofiy@seriouscake.net>
2017-10-23 18:11:34 +0300

    bat files

--- classiclogic/ClassicLogicEngine/copysrc.bat
+++ classiclogic/ClassicLogicEngine/copysrc.bat
@@ -1,6 +1,4 @@
-@SET SRC_DIR="ClassicLogicEngine\"
+@SET SRC_DIR="ClassicLogicEngine\src"
 @SET COPY_TO="..\..\game\Assets\ThirdParty\ClassicLogicEngine\src\"
 
-@xcopy %SRC_DIR%Engine %COPY_TO%Engine\ /S
-@xcopy %SRC_DIR%Outputs %COPY_TO%Outputs\ /S
-@xcopy %SRC_DIR%Utils %COPY_TO%Utils\ /S
+@xcopy %SRC_DIR% %COPY_TO% /S
---
+++ classiclogic/ClassicLogicEngine/makelink.bat
@@ -0,0 +1,4 @@
+@SET SRC_DIR="ClassicLogicEngine\src"
+@SET LINK_TO="..\..\game\Assets\ThirdParty\ClassicLogicEngine\src\"
+
+@mklink /D /J %LINK_TO% %SRC_DIR%
