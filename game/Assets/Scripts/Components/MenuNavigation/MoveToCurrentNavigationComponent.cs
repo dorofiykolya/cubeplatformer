ea07dd95b644f437f9d9e3b495efca6a2c6082b9
dorofiy <dorofiykolya@gmail.com>
2018-01-08 16:56:06 +0200

    curve menu navigation

--- game/Assets/Scripts/Components/MenuNavigation/MoveToCurrentNavigationComponent.cs
+++ game/Assets/Scripts/Components/MenuNavigation/MoveToCurrentNavigationComponent.cs
@@ -6,11 +6,17 @@ namespace Game.Components.MenuNavigation
   {
     public MenuNavigationComponent Navigation;
     public Transform Target;
-    public float Speed = 1f;
+    [Tooltip("seconds")]
+    public float NavigationTime = 1f;
+    public AnimationCurve Curve;
 
     public bool FreezeX;
     public bool FreezeY;
     public bool FreezeZ;
+
+    private NavigationComponent _navigationComponent;
+    private float _passedTime;
+    private Vector3 _prevPosition;
 
     private void Awake()
     {
@@ -25,10 +31,29 @@ namespace Game.Components.MenuNavigation
     {
       if (Target != null && Navigation != null && Navigation.Current != null)
       {
+        if (_navigationComponent != Navigation.Current)
+        {
+          _navigationComponent = Navigation.Current;
+          _prevPosition = Target.position;
+          _passedTime = 0;
+        }
+
+        _passedTime += Time.deltaTime;
+
+        var ratio = Mathf.Clamp01(_passedTime / NavigationTime);
+        var value = Curve.Evaluate(ratio);
+
         var position = Target.position;
         var current = Navigation.Current;
+
         var currentTransform = current.CameraPivotTransform != null ? current.CameraPivotTransform : current.Target;
-        var newPosition = Vector3.MoveTowards(Target.position, currentTransform.position, Speed);
+
+        var curPos = currentTransform.position;
+
+        var delta = curPos - _prevPosition;
+
+        var newPosition = _prevPosition + new Vector3(delta.x * value, delta.y * value, delta.z * value);
+
         if (FreezeX) newPosition.x = position.x;
         if (FreezeY) newPosition.y = position.y;
         if (FreezeZ) newPosition.z = position.z;
