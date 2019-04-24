a61a1160348a242c1d54222ba0c971ea0cf07337
dorofiy <dorofiykolya@gmail.com>
2019-04-24 23:11:58 +0300

    fixes

--- game/Assets/Scripts/UI/Components/UIMainMenuComponent.cs
+++ game/Assets/Scripts/UI/Components/UIMainMenuComponent.cs
@@ -1,13 +1,27 @@
 ﻿using Game.Components.MenuNavigation;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace Game.UI.Components
 {
   public class UIMainMenuComponent : MonoBehaviour
   {
+    private static bool _firstStart = true;
+
     public Camera MainCamera;
     public Camera[] Cameras;
     public MenuNavigationComponent Navigation;
+    public PlayableDirector PlayableDirector;
+
+    private void Awake()
+    {
+      PlayableDirector.playOnAwake = _firstStart;
+      if (!_firstStart)
+      {
+        PlayableDirector.Stop();
+      }
+      _firstStart = false;
+    }
 
     public void ActiveCameras(bool value)
     {
